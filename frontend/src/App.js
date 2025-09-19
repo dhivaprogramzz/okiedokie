087f9378d9792a4e86b33f5fb5f2710b5ae539cd
Dhivyarethika <dhividhivi19@gmail.com>
2025-09-19 17:06:25 +0530

    Display backend message in App.js frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 //import './App.css';
 
+import React,{ useEffect,useState } from "react";
+
 function App() {
+  const [message,setMessage]=useState("");
+
+  useEffect(()=> {
+    fetch("http://127.0.0.1:5000/api/hello")
+    .then((res) =>res.json())
+    .then((data) => setMessage(data.message))
+    .catch((err) => console.error(err));
+  },[]);
   return (
     <div className="App">
       <h1>Hello from Okiedokie Frontend!</h1>
+       <p>{message}</p> {/*displays the backend message here */}
     </div>
   );
 }
